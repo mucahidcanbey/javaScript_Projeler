0da237d490619026532cb95d471745de73624034
Mucahid Canbey <canbeylermucahid@gmail.com>
2023-12-08 00:45:45 +0300

    'addNew' button a aksiyon eklendi.

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-// Sayfadaki bir HTML öğesini seçiyoruz.
+// 'hexInputContainer' Sayfadaki bir HTML öğesini seçiyoruz. 
 const hexInputContainer = document.querySelector("#hexInputContainer")
+// '#hexColorFrom' Sayfadaki bir HTML öğesini seçiyoruz. 
+const hexFrom = document.querySelector("#hexColorFrom")
+// 'addNew' Sayfadaki bir Button öğesi seçiyoruz. 
+const addNew = document.querySelector("#addNew")
 
-// Yeni bir div öğesi oluşturuyoruz.
-const newDivElement = document.createElement('div')
+// Sayacı başlat
+let counter = 1
 
-// Oluşturduğumuz div öğesine "col-sm" sınıfını ekliyoruz.
-newDivElement.classList.add('col-sm')
+// "addNew"  butonuna tıklayınca gerçekleşek aksiyon.
+addNew.addEventListener("click", () => {
+    console.log("click")
+    // Sayacı artır
+    counter += 1
 
-// Oluşturduğumuz div öğesinin içine bir şeyler eklemek için, innerHTML özelliğini kullanıyoruz.
-newDivElement.innerHTML = `
-<label for="colorInput" class="form-label">Hex Color</label>
-<input type="text" 
+    hexInputContainer.append(newHexColorInput(counter))
+})
+
+function newHexColorInput(itemCount) {
+
+    // Yeni bir div öğesi oluşturuyoruz.
+    const newDivElement = document.createElement('div')
+    // Oluşturduğumuz div öğesine "col-sm" sınıfını ekliyoruz.
+    newDivElement.classList.add('col-sm')
+    // Oluşturduğumuz div öğesinin içine bir şeyler eklemek için, innerHTML özelliğini kullanıyoruz.
+    newDivElement.innerHTML = `
+<label for="colorInput-${itemCount}" class="form-label">Hex Color${itemCount}</label>
+<input
+id="colorInput-${itemCount}" 
+type="text" 
 minlength="6" maxlength="6" 
 class="form-control"  
-name="color1"
+name="color-${itemCount}"
 >
 `
+    return newDivElement
+}
+
 // Oluşturduğumuz div öğesini hexInputContainer'a ekliyoruz.
-hexInputContainer.append(newDivElement)
+hexInputContainer.append(newHexColorInput(counter))
+
+
+hexFrom.addEventListener("submit", (event) => {
+
+})

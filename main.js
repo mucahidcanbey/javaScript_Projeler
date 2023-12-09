2bad85aa42f3029ec5850ce609b98f945e091210
Mucahid Canbey <canbeylermucahid@gmail.com>
2023-12-09 03:35:31 +0300

    Form icindeki Bilgilerin localStorage a Yazilmasi

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 // 'hexInputContainer' Sayfadaki bir HTML öğesini seçiyoruz. 
 const hexInputContainer = document.querySelector("#hexInputContainer")
 // '#hexColorFrom' Sayfadaki bir HTML öğesini seçiyoruz. 
-const hexFrom = document.querySelector("#hexColorFrom")
+const hexForm = document.querySelector("#hexColorForm")
 // 'addNew' Sayfadaki bir Button öğesi seçiyoruz. 
 const addNew = document.querySelector("#addNew")
 
-// Sayacı başlat
-let counter = 1
 
+let counter = 1
 // "addNew"  butonuna tıklayınca gerçekleşek aksiyon.
 addNew.addEventListener("click", () => {
     console.log("click")
-    // Sayacı artır
+    
     counter += 1
 
     hexInputContainer.append(newHexColorInput(counter))
@@ -38,9 +37,22 @@ name="color-${itemCount}"
 }
 
 // Oluşturduğumuz div öğesini hexInputContainer'a ekliyoruz.
-hexInputContainer.append(newHexColorInput(counter))
 
+hexInputContainer.append( newHexColorInput(counter) )
 
-hexFrom.addEventListener("submit", (event) => {
+let localStorageColors = localStorage.getItem("colors") ? JSON.parse(localStorage.getItem("colors")) : []
 
+hexForm.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  let colors = []
+  Array.from(event.target.elements).forEach(item => {
+    if (item.type === "text") {
+      colors.push(item.value)
+    }
+  })
+  localStorageColors.push(colors)
+  
+  localStorage.setItem("colors", JSON.stringify(localStorageColors))
+  hexForm.reset()
 })
